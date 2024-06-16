b2faa20f5ba839e5c50f620cc71a3a39782a8429
AJ Villalobos <hello@ajvillalobos.com>
2024-06-16 11:16:46 +0800

    Add endpoints for /images, /images/:id for creating and displaying

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,27 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { Router } from 'itty-router';
+import getImages from './handlers/get_images';
+import createImage from './handlers/create_image';
+import getSingleImage from './handlers/get_single_image';
+
+const router = Router();
+
+router
+	.get('/images', getImages)
+	.get('/images/:id', getSingleImage)
+	.post('/images', createImage)
+	.get('*', () => new Response('Not Found', { status: 404 }));
+
+export interface Env {}
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		return router.fetch(request);
 	},
 };
