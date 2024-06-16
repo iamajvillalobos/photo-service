efab3cb23ad78d8b9b6e1654c115d77b2558d1cc
AJ Villalobos <hello@ajvillalobos.com>
2024-06-16 12:36:21 +0800

    Add test with vitest

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,9 @@
-// test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import {
+	env,
+	createExecutionContext,
+	waitOnExecutionContext,
+	SELF,
+} from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import worker from '../src/index';
 
@@ -7,19 +11,97 @@ import worker from '../src/index';
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+describe('Photo service', () => {
+	it('returns a 404 if a non-existent endpoint is called', async () => {
+		const response = await SELF.fetch(
+			'http://www.example.com/invalid-endpoint'
+		);
+
+		expect(response.status).toEqual(404);
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+	describe('GET /images', () => {
+		it('should return a 200 OK response', async () => {
+			const response = await SELF.fetch('http://www.example.com/images');
+			expect(response.status).toEqual(200);
+		});
+
+		it('should return images in the response', async () => {
+			const response = await SELF.fetch('http://www.example.com/images');
+			const json = await response.json();
+
+			expect(json).toEqual(
+				expect.arrayContaining([
+					expect.objectContaining({
+						id: 3,
+						url: 'https://bar.com/img1',
+						author: 'Lara Lobster',
+					}),
+				])
+			);
+		});
+
+		it('should return a set number of images if count is provided', async () => {
+			const response = await SELF.fetch(
+				'http://www.example.com/images?count=2'
+			);
+			const json = await response.json();
+
+			expect(json).toHaveLength(2);
+		});
+	});
+});
+
+describe('GET /images/:id', () => {
+	it('should return a 200 OK response', async () => {
+		const response = await SELF.fetch('http://www.example.com/images/1');
+		expect(response.status).toEqual(200);
+	});
+
+	it('should return the correct image', async () => {
+		const response = await SELF.fetch('http://www.example.com/images/1');
+		const json = await response.json();
+
+		expect(json).toEqual(
+			expect.objectContaining({
+				id: 1,
+				url: 'https://foo.com/img1',
+				author: 'Bart Simpson',
+			})
+		);
+	});
+});
+
+describe('POST /images', () => {
+	it('should return a 201 response code', async () => {
+		const payload = {
+			id: 4,
+			url: 'https://example.com/some_image.png',
+			author: 'Lia',
+		};
+
+		const response = await SELF.fetch('http://www.example.com/images', {
+			method: 'POST',
+			body: JSON.stringify(payload),
+		});
+
+		expect(response.status).toEqual(201);
+	});
+
+	it('should return the created image in the response', async () => {
+		const newImage = {
+			id: 4,
+			url: 'https://example.com/some_image.png',
+			author: 'Lia',
+		};
+
+		const response = await SELF.fetch('http://www.example.com/images', {
+			method: 'POST',
+			body: JSON.stringify(newImage),
+		});
+
+		const json = await response.json();
+
+		expect(json).toEqual(expect.objectContaining(newImage));
 	});
 });
